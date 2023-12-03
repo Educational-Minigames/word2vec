de6e6149231a4845d9300ebb6592f6240d43e93d
AlieNiT <ali.blade.1380@gmail.com>
2023-12-03 14:34:55 -0700

    Cleaning the code

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import pandas as pd
 import plotly.express as px
 import streamlit as st
-import numpy as np
+import pandas as pd
 import json
-from utils import *
 
 PLOT_HEIGHT = 600
 
@@ -11,22 +9,22 @@ config = st.experimental_get_query_params()
 ADD_WORDS = (config.get('add_words', 'false')) != 'false'
 SHOW_RELATIONS = (config.get('show_relations', 'false')) != 'false'
 SHOW_COLORS = (config.get('show_colors', 'false')) != 'false'
-config = {'modeBarButtonsToRemove': 
+config = {'modeBarButtonsToRemove':
           ['toImage',
            'lasso2d',
            'zoom2d',
            'select2d',
            'autoScale2d',
            ],
-           'displaylogo': False}
+          'displaylogo': False}
+
+
 # Function to create and return the scatterplot figure with arrows for a selected relation
 def create_scatterplot_with_arrows(data, relation_data=[]):
     if SHOW_COLORS:
         fig = px.scatter(data, x="x", y="y", text="word", color="cluster")
     else:
         fig = px.scatter(data, x="x", y="y", text="word")
-    
-
 
     fig.update_traces(
         marker=dict(size=7),
@@ -39,8 +37,10 @@ def create_scatterplot_with_arrows(data, relation_data=[]):
     # Add arrows for each word pair in the selected relation
     for pair in relation_data:
         word1, word2 = pair
-        x1, y1 = data[data['word'] == word1]['x'].values[0], data[data['word'] == word1]['y'].values[0]
-        x2, y2 = data[data['word'] == word2]['x'].values[0], data[data['word'] == word2]['y'].values[0]
+        x1, y1 = data[data['word'] ==
+                      word1]['x'].values[0], data[data['word'] == word1]['y'].values[0]
+        x2, y2 = data[data['word'] ==
+                      word2]['x'].values[0], data[data['word'] == word2]['y'].values[0]
 
         # Add an arrow annotation
         fig.add_annotation(
@@ -67,12 +67,9 @@ def create_scatterplot_with_arrows(data, relation_data=[]):
 
     return fig
 
+
 # Load the data from the CSV file
 raw_data = pd.read_csv("data.csv")
-
-# Delete the 'Unnamed: 0' column
-if 'Unnamed: 0' in raw_data.columns:
-    del raw_data['Unnamed: 0']
 
 # Load relationship data from the "groups.json" file
 with open("groups.json", "r", encoding="utf-8") as json_file:
@@ -130,11 +127,9 @@ if ADD_WORDS:
     st.sidebar.header("کلمه جدید")
     with st.form("my-form"):
         word_to_add = st.sidebar.text_input("کلمه جدید وارد کنید:")
-        add_button = st.sidebar.button("اضافه کردن کلمه")    
-    
-    
-    reset_button = st.sidebar.button("حذف کلمات جدید")
+        add_button = st.sidebar.button("اضافه کردن کلمه")
 
+    reset_button = st.sidebar.button("حذف کلمات جدید")
 
     # Initialize 'added_words' in session state or if reset button to clear 'added_words'
     if 'added_words' not in st.session_state or reset_button:
@@ -144,20 +139,23 @@ if ADD_WORDS:
     if add_button and word_to_add:
         if word_to_add in plot_data['word'].values or word_to_add in [data_point['word'] for data_point in st.session_state.added_words]:
             st.sidebar.warning(f"کلمه '{word_to_add}' قبلا اضافه شده.")
-        elif word_to_add in hidden_data['word'].values :
+        elif word_to_add in hidden_data['word'].values:
             x = hidden_data[hidden_data['word'] == word_to_add]['x'].values[0]
             y = hidden_data[hidden_data['word'] == word_to_add]['y'].values[0]
-            st.session_state.added_words.append({'word': word_to_add, 'x': x, 'y': y, 'cluster': 'جدید'})  
+            st.session_state.added_words.append(
+                {'word': word_to_add, 'x': x, 'y': y, 'cluster': 'جدید'})
         else:
             st.sidebar.warning(f"کلمه '{word_to_add}' در لغت‌نامه‌ نیست.")
-    plot_data = pd.concat([plot_data, pd.DataFrame(st.session_state.added_words)], ignore_index=True)
+    plot_data = pd.concat([plot_data, pd.DataFrame(
+        st.session_state.added_words)], ignore_index=True)
 else:
     plot_data = plot_data
 
 # Dropdown menu to select a relation
 if SHOW_RELATIONS:
     st.sidebar.header("انتخاب ارتباط")
-    selected_relation = st.sidebar.selectbox("یک ارتباط انتخاب کنید.", ['هیچ کدام'] + list(relationship_data.keys()))
+    selected_relation = st.sidebar.selectbox(
+        "یک ارتباط انتخاب کنید.", ['هیچ کدام'] + list(relationship_data.keys()))
 
     # Process and display the selected relation with arrows if it exists
     if selected_relation:
